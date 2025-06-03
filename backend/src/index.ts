f09465e08e4511d7dd350f126a89323e7b2df544
yuofi <pavel.d.yulov@gmail.com>
2025-06-03 19:34:59 +0300

    fixed origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,7 @@ void (async () => {
     
     app.use(
       cors({
-        origin: ["http://localhost:5173"],
+        origin: ["http://https://russian-stress-practice.onrender.com/", "http://localhost:5173"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: [
